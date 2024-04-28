21fedad0cd23ff767c2beaee7c5aa26be8b0fa82
SungeunAmberKim <sungeunkim@cpp.edu>
2024-04-27 21:02:45 -0700

    started part 2

---
+++ src/main/java/SongService.java
@@ -0,0 +1,6 @@
+import java.util.List;
+public interface SongService {
+    Song searchById(Integer songID);
+    List<Song> searchByTitle(String title);
+    List<Song> searchByAlbum(String album);
+}
